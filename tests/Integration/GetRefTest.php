229d13de9b044774c3a2058be7090977ae2e6909
v.stepanenko <mail@vitaliy.in>
2017-03-14 20:23:09 +0200

    Refactoring GetRefTest

--- tests/Integration/GetRefTest.php
+++ tests/Integration/GetRefTest.php
@@ -2,6 +2,7 @@
 
 namespace Nayjest\DI\Test\Integration;
 
+use Nayjest\DI\Definition\ItemDefinition;
 use Nayjest\DI\Hub;
 use PHPUnit\Framework\TestCase;
 
@@ -10,16 +11,25 @@ class GetRefTest extends TestCase
 {
     public function test()
     {
-        $hub = new Hub();
-        $hub->builder()->define('item', 1);
+        $hub = new Hub([
+            new ItemDefinition('item',1)
+        ]);
         $ref = &$hub->get('item');
+        $hub->set('item', 2);
+        $this->assertEquals(2, $ref);
+    }
+
+    public function testExternalChange()
+    {
+        $hub = new Hub([
+            new ItemDefinition('item',1)
+        ]);
+        $ref = &$hub->get('item');
+
         // THIS OPERATION IS NOT ALLOWED
         // CONSIDER ITEMS EXTRACTED BY REFERENCE READONLY,
         // CHAGE VALUES ONLY VIA HUB
         $ref = 3;
         $this->assertEquals(3, $hub->get('item'));
-
-        $hub->set('item', 2);
-        $this->assertEquals(2, $ref);
     }
 }
